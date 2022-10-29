783dd28030f2776ab06119ca66648cf329dfccc4
Alex <8125011+proteriax@users.noreply.github.com>
2022-10-28 21:54:38 -0400

    Make TypeScript build

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import { defineConfig } from 'rollup';
+import ts from 'rollup-plugin-ts';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'lib/ical/module.js',
+export default defineConfig({
+  input: 'lib/ical/module.ts',
+  plugins: [
+    nodeResolve({
+      extensions: ['.js', '.ts']
+    }),
+    ts({
+      transpileOnly: true
+    })
+  ],
   output: [
-    { file: 'dist/ical.js', format: 'es', exports: 'default' },
     {
-      file: 'dist/ical.min.js',
+      file: 'dist/ical.js',
       format: 'es',
-      exports: 'default',
-      plugins: [terser()]
+      exports: 'default'
     },
     {
-      file: 'dist/ical.es5.cjs',
-      exports: 'default',
-      format: 'cjs',
-      plugins: [getBabelOutputPlugin()]
-    },
-    {
-      file: 'dist/ical.es5.min.cjs',
-      exports: 'default',
-      format: 'cjs',
-      plugins: [getBabelOutputPlugin(), terser()]
+      file: 'dist/ical.cjs',
+      format: 'commonjs',
+      exports: 'default'
     }
   ]
-};
+});
